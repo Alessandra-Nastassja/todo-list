73fd59ab57579409b35082fe11c84bf4943d4f16
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2023-09-04 21:03:01 -0300

    feat: it was setting style guide

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { Header } from "./components/Header";
+
+import './global.css';
+import styles from './App.module.css';
+
 export function App() {
-  return (<></>)
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        
+      </div>
+    </>
+  )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header(){
+  return (
+    <header>
+      <img src="" alt="todo" />
+    </header>
+  )
+}
