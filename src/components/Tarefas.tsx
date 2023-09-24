51b9f7544484ac9b969240d1a4da8d0e36c8ded1
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2023-09-23 23:16:50 -0300

    feat: adjustment in the style

--- src/components/Tarefas.tsx
+++ src/components/Tarefas.tsx
@@ -2,15 +2,23 @@ import styles from './Tarefas.module.css';
 
 export function Tarefas(){
   return (
-    <section className={styles.tarefasContent}>
-      <div className={styles.tarefasCriadas}>
-        <p>Tarefas criadas</p>
-        <span>0</span>
+    <section>
+      <div className={styles.tarefasContent}>
+        <div className={styles.tarefasCriadas}>
+          <p>Tarefas criadas</p>
+          <span>0</span>
+        </div>
+        <div className={styles.tarefasConcluidas}>
+          <p>Tarefas concluídas</p>
+          <span>0</span>
+        </div>
       </div>
-      <div className={styles.tarefasConcluidas}>
-        <p>Tarefas concluídas</p>
-        <span>0</span>
+
+      <div className={styles.semTarefas}>
+        <strong>Você ainda não tem tarefas cadastradas</strong>
+        <p>Crie tarefas e organize seus itens a fazer</p>
       </div>
+
     </section>
   )
 }
