d72d783118209f9f2f500f0358b8b6df8f1e9291
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2023-09-30 22:26:36 -0300

    feat: created logic of the add new task

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,34 @@
 import { PlusCircle } from 'phosphor-react';
 import styles from './Search.module.css';
+import { ChangeEvent, useState } from 'react';
 
 export function Search() {
+  const [task, setTask] = useState('');
+  const [newTask, setNewTask] = useState([]);
+
+  function handleTask(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('');
+    setTask(event.target.value);
+  }
+
+  function addNewTask() {
+    setNewTask([...task, newTask]);
+
+    console.log(task);
+    
+  }
+
   return (
     <section className={styles.searchContent}>
       <div className={styles.search}>
         <input 
           type="text" 
-          placeholder='Adicione uma nova tarefa' />
-        <button type="submit" className={styles.buttonCriarTarefa}>
+          placeholder='Adicione uma nova tarefa'
+          value={task}
+          onChange={handleTask}/>
+        <button type="submit" 
+          className={styles.buttonCriarTarefa}
+          onClick={addNewTask}>
           Criar
           <PlusCircle size={16} /> 
         </button>
